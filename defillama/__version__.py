c01634f63817e5f6d4e2ff1ccd13e5b0c5cfc66c
Pratyush Shukla <ps4534@nyu.edu>
2022-04-17 11:00:09 -0400

    Updated DeFi Llama library
    Modified init and version files.

--- defillama/__version__.py
+++ defillama/__version__.py
@@ -1,10 +1,9 @@
 
 __title__ = 'DeFiLlama'
-__description__ = 'Unofficial DeFi Llama API client.'
-__url__ = 'https://github.com/itzmestar/DeFiLlama'
-__version__ = '1.1.0'
-__build__ = 0x010001
-__author__ = 'Tarique Anwer'
-__author_email__ = 'itzmetariq@gmail.com'
+__description__ = 'Unofficial Python wrapper for DeFi Llama API client using PyCurl module.'
+__url__ = 'https://github.com/the-praxs/DeFiLlama'
+__version__ = '1.0'
+__author__ = 'Pratyush Shukla'
+__author_email__ = 'ps4534@nyu.edu'
 __license__ = 'Apache License 2.0'
-__copyright__ = 'Copyright 2021 Tarique Anwer'
+__copyright__ = 'Copyright 2022 Pratyush Shukla'
